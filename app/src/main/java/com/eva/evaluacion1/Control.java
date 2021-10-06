e9a83c86204254f9e6cf56cb3731dcb3b87a5c6c
Chentro <vasquezsaavedrasergio@gmail.com>
2021-10-05 22:02:23 -0300

    Initial commit

--- app/src/main/java/com/eva/evaluacion1/Control.java
+++ app/src/main/java/com/eva/evaluacion1/Control.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Control extends AppCompatActivity {
+
+    TextView TOTAL, HOMBRES, MUJERES;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+
+        TOTAL = (TextView) findViewById(R.id.lbl_total);
+        HOMBRES = (TextView) findViewById(R.id.lbl_hom);
+        MUJERES = (TextView) findViewById(R.id.lbl_muj);
+
+        Bundle B = getIntent().getExtras();
+        String GeneroRecibido = B.getString("Genero");
+
+
+
+
     }
 
     public void Agregar(View v){
